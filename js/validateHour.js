588da1aa6916fef35eaf41ac764d0f21c5e892b6
jarregui92 <jarregui92@gmail.com>
2023-12-07 22:13:02 -0300

    Ahora el sistema controla si aun hay tiempo para reservas y de no serlo permite agendarse desde el dia siguiente a la hora de apertura de no estar reservada aun

--- js/validateHour.js
+++ js/validateHour.js
@@ -4,12 +4,27 @@ import siteConfig from '../siteConfig.js';
 //SE OBTIENE LA CONFIGURACION DEL HORARIO DE LA EMPRESA
 let scheduleConfiguration = siteConfig.scheduleConfiguration;
 
+  //SE OBTIENE LA HORA ACTUAL Y SE LE SUMA 1 PARA QUE NO SE PUEDA AGENDAR CON MENOS DE UNA HORA DE ANTELACION
+  let now = new Date();
+  let nowHour = now.getHours();
+  let minSelectableHour = nowHour + 1;
+
+ //SE OBTIENE LA HORA ACTUAL Y SE LE SUMA 1 PARA QUE NO SE PUEDA AGENDAR CON MENOS DE UNA HORA DE ANTELACION
+ let minDate = new Date(now);
+ if (minSelectableHour > 17) {
+   // SI LA HORA MINIMA SELECCIONABLE ES DESPUES DE LAS 17, SE ESTABKECE LA FECHA MINIMA EL DIA DE MAÑANA
+   minDate.setDate(now.getDate() + 1);
+  } else {
+    //DE LO CONTARRIO, LA FECHA MINIMA ES HOY
+    minDate.setDate(now.getDate());
+ }
 
 const datePicker = flatpickr("#datepicker", {
   enableTime: false,
   dateFormat: "Y-m-d",
-  minDate: "today",
-  maxDate: new Date().fp_incr(scheduleConfiguration.maxDaysForReservations),
+  minDate: minDate,
+  // fp_incr ES UNA FUNCION PROPIA DE FLATPICKR QUE INCREMENTA UNA FECHA, LA CUAL LE RESTO UNO PARA Q SEAN 10 DIAS
+  maxDate: minDate.fp_incr(scheduleConfiguration.maxDaysForReservations -1),
   onChange: function(selectedDates) {
     // OBTENER LA FECHA SELECCIONADA
     let selectedDate = selectedDates[0].toISOString().split("T")[0];
@@ -46,9 +61,9 @@ const datePicker = flatpickr("#datepicker", {
       option.text = i < 10 ? `0${i}:00` : `${i}:00`;
 
       // SE DESHABILITA LA OPCION SI LA HORA ESTA RESERVADA Y SE AGREGA UN TEXTO AL LADO EXPLICANDO PQ NO PUEDE SER SELECCIONADA
-      if (reservationHours.includes(i)) {
+      if (reservationHours.includes(i) || i < minSelectableHour) {
         option.disabled = true;
-        option.text += ' (Hora ocupada)';
+        option.text += ' (Hora no disponible)';
       }
 
       timePicker.add(option);
